3f3e4ebe125f91c8718af9fbf456148d2121985c
radu.solca <solca.radu@yahoo.com>
2017-01-06 20:03:33 +0200

    Add Utils.py

---
+++ Utils.py
@@ -0,0 +1,18 @@
+def getNextTag(posTag, index, whatIs):
+        try:
+            i = index
+            while(i < len(posTag)):
+                i += 1
+                tag = posTag[i]
+                if tag[1] in whatIs:
+                    i += 1
+                    while(i<len(posTag)):
+                        if posTag[i] not in whatIs:
+                            break
+                        else:
+                            tag = posTag[i]
+                            i+=1
+                    return tag
+            return None
+        except Exception:
+            return None
